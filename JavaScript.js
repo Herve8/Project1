446265b4cb6b4ea244b9bb6fbe7b18809f3e303a
renubala <renubala@192-168-1-19.tpgi.com.au>
2020-12-16 16:32:14 +0800

    addfile

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+var a = [
+  "We're up all night 'til the sun",
+  "We're up all night to get some",
+  "We're up all night for good fun",
+  "We're up all night to get lucky"
+];
+
+
+var a2 = a.map(function (s) { return s.length });
+
+
+var a3 = a.map( s => s.length);
